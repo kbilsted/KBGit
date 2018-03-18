ce8eee90d6200c68a88f6d1bbd6de6ecc22e82b1
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2018-03-18 18:58:54 +0100

    extract RepoBuilder

--- kbgit.tests/KBGitTests.cs
+++ kbgit.tests/KBGitTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using KbgSoft.KBGit;
 using Xunit;
@@ -240,81 +239,6 @@ visitblob FeatureVolvo\car.txt
 			repoBuilder.NewBranch("featurebranch");
 			Assert.Equal(@"* a/featurebranch
   a/master", repoBuilder.Git.Branch());
-		}
-	}
-
-	public class RepoBuilder
-	{
-		readonly string basePath;
-		readonly string repositoryName;
-		public KBGit Git;
-
-		public RepoBuilder(string repositoryName, string basePath)
-		{
-			this.basePath = basePath;
-			this.repositoryName = repositoryName;
-		}
-
-		public KBGit BuildEmptyRepo()
-		{
-			Git = new KBGit(repositoryName, basePath);
-			Git.Init();
-			return Git;
-		}
-
-		public RepoBuilder EmptyRepo()
-		{
-			Git = new KBGit(repositoryName, basePath);
-			Git.Init();
-			return this;
-		}
-
-		public KBGit Build2Files3Commits()
-		{
-			Git = BuildEmptyRepo();
-
-			AddFile("a.txt", "aaaaa");
-			Git.Commit("Add a", "kasper", new DateTime(2017,1,1,1,1,1), Git.ScanFileSystem());
-
-			AddFile("b.txt", "bbbb");
-			Git.Commit("Add b", "kasper", new DateTime(2017, 2, 2, 2, 2, 2), Git.ScanFileSystem());
-
-			AddFile("a.txt", "v2av2av2av2a");
-			Git.Commit("Add a2", "kasper", new DateTime(2017, 3, 3, 3, 3, 3), Git.ScanFileSystem());
-
-			return Git;
-		}
-
-		public RepoBuilder AddFile(string path) => AddFile(path, Guid.NewGuid().ToString());
-		public RepoBuilder AddFile(string path, string content)
-		{
-			var filepath = Path.Combine(Git.CodeFolder, path);
-			new FileInfo(filepath).Directory.Create();
-
-			File.WriteAllText(filepath, content);
-			return this;
-		}
-
-		public string ReadFile(string path)
-		{
-			return File.ReadAllText(Path.Combine(Git.CodeFolder, path));
-		}
-
-		public RepoBuilder DeleteFile(string path)
-		{
-			File.Delete(Path.Combine(basePath, path));
-			return this;
-		}
-
-		public Id Commit()
-		{
-			return Git.Commit("some message", "author", DateTime.Now, Git.ScanFileSystem());
-		}
-
-		public RepoBuilder NewBranch(string branch)
-		{
-			Git.CheckOut_b(branch);
-			return this;
 		}
 	}
 }
---
+++ kbgit.tests/RepoBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using KbgSoft.KBGit;
+
+namespace kbgit.tests
+{
+	public class RepoBuilder
+	{
+		readonly string basePath;
+		readonly string repositoryName;
+		public KBGit Git;
+
+		public RepoBuilder(string repositoryName, string basePath)
+		{
+			this.basePath = basePath;
+			this.repositoryName = repositoryName;
+		}
+
+		public KBGit BuildEmptyRepo()
+		{
+			Git = new KBGit(repositoryName, basePath);
+			Git.Init();
+			return Git;
+		}
+
+		public RepoBuilder EmptyRepo()
+		{
+			Git = new KBGit(repositoryName, basePath);
+			Git.Init();
+			return this;
+		}
+
+		public KBGit Build2Files3Commits()
+		{
+			Git = BuildEmptyRepo();
+
+			AddFile("a.txt", "aaaaa");
+			Git.Commit("Add a", "kasper", new DateTime(2017,1,1,1,1,1), Git.ScanFileSystem());
+
+			AddFile("b.txt", "bbbb");
+			Git.Commit("Add b", "kasper", new DateTime(2017, 2, 2, 2, 2, 2), Git.ScanFileSystem());
+
+			AddFile("a.txt", "v2av2av2av2a");
+			Git.Commit("Add a2", "kasper", new DateTime(2017, 3, 3, 3, 3, 3), Git.ScanFileSystem());
+
+			return Git;
+		}
+
+		public RepoBuilder AddFile(string path) => AddFile(path, Guid.NewGuid().ToString());
+		public RepoBuilder AddFile(string path, string content)
+		{
+			var filepath = Path.Combine(Git.CodeFolder, path);
+			new FileInfo(filepath).Directory.Create();
+
+			File.WriteAllText(filepath, content);
+			return this;
+		}
+
+		public string ReadFile(string path)
+		{
+			return File.ReadAllText(Path.Combine(Git.CodeFolder, path));
+		}
+
+		public RepoBuilder DeleteFile(string path)
+		{
+			File.Delete(Path.Combine(basePath, path));
+			return this;
+		}
+
+		public Id Commit()
+		{
+			return Git.Commit("some message", "author", DateTime.Now, Git.ScanFileSystem());
+		}
+
+		public RepoBuilder NewBranch(string branch)
+		{
+			Git.CheckOut_b(branch);
+			return this;
+		}
+	}
+}
