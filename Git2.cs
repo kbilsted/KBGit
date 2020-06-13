494ca0e2b77502ccab705d47ccc7e25806bc496c
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2020-06-13 19:06:46 +0200

    implement git init

---
+++ Git2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace KbgSoft.KBGit
+{
+    public class Git2
+    {
+        public readonly string RootPath;
+
+        public Git2(string rootPath)
+        {
+            RootPath = rootPath;
+        }
+
+        public string Init()
+        {
+            new DirectoryInfo(Path.Combine(RootPath, ".git")).Create();
+            new DirectoryInfo(Path.Combine(RootPath, ".git", "refs", "heads")).Create();
+            new DirectoryInfo(Path.Combine(RootPath, ".git", "objects")).Create();
+            new DirectoryInfo(Path.Combine(RootPath, ".git", "index")).Create();
+            File.WriteAllText(Path.Combine(RootPath, ".git", "HEAD"), "ref: refs/heads/master");
+            return "Initialized empty Git repository";
+        }
+
+        public string Commit(string commitMessage)
+        {
+            return "";
+        }
+
+        public string CreateBranch(string name)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// return all branches and highlight current branch: "git branch"
+        /// </summary>
+        public string ListBranches()
+        {
+            return "";
+        }
+    }
+
+}
---
+++ kbgit.tests/Git2Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using KbgSoft.KBGit;
+using Xunit;
+
+namespace kbgit.tests
+{
+    public class Git2Tests
+    {
+        Git2 MakeEmptyRepo()
+        {
+            var name = $@"c:\temp\kbggit{DateTime.Now.Ticks}";
+            Directory.CreateDirectory(name);
+            return new Git2(name);
+        }
+
+        Git2 InitEmptyRepo()
+        {
+            var git = MakeEmptyRepo();
+            git.Init();
+            return git;
+        }
+
+        [Fact]
+        public void When_init_Then_create_git_folder()
+        {
+            var git = MakeEmptyRepo();
+            
+            var msg = git.Init();
+                
+            msg.Should().Be("Initialized empty Git repository");
+            new DirectoryInfo(Path.Combine(git.RootPath, ".git")).Exists.Should().BeTrue();
+            new DirectoryInfo(Path.Combine(git.RootPath, ".git/refs/heads")).Exists.Should().BeTrue();
+            new DirectoryInfo(Path.Combine(git.RootPath, ".git/objects")).Exists.Should().BeTrue();
+            new DirectoryInfo(Path.Combine(git.RootPath, ".git/index")).Exists.Should().BeTrue();
+            File.ReadAllText(Path.Combine(git.RootPath, ".git/HEAD")).Should().Be("ref: refs/heads/master");
+        }
+
+        [Fact]
+        public void When_list_branches_Then_return_empty()
+        {
+            var git = InitEmptyRepo();
+
+            new DirectoryInfo(Path.Combine(git.RootPath, ".git")).Exists.Should().BeTrue();
+        }
+
+
+    }
+}
