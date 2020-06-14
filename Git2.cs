2e484c1d6bb089c35321d62cf93f1b16dd8cebba
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2020-06-14 20:49:24 +0200

    git add

--- Git2.cs
+++ Git2.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
 using System.Text;
 
-namespace KbgSoft.KBGit
+namespace KbgSoft.KBGit2
 {
     public class Git2
     {
@@ -18,11 +21,38 @@ namespace KbgSoft.KBGit
         {
             new DirectoryInfo(Path.Combine(RootPath, ".git")).Create();
             new DirectoryInfo(Path.Combine(RootPath, ".git", "refs", "heads")).Create();
-            new DirectoryInfo(Path.Combine(RootPath, ".git", "objects")).Create();
-            new DirectoryInfo(Path.Combine(RootPath, ".git", "index")).Create();
-            File.WriteAllText(Path.Combine(RootPath, ".git", "HEAD"), "ref: refs/heads/master");
+            new DirectoryInfo(Path.Combine(RootPath, ".git/objects")).Create();
+            File.WriteAllText(Path.Combine(RootPath, ".git/index"), "");
+            File.WriteAllText(Path.Combine(RootPath, ".git/HEAD"), "ref: refs/heads/master");
             return "Initialized empty Git repository";
         }
+
+        /// <summary>
+        /// stage a file
+        /// https://mincong.io/2018/04/28/git-index/#:~:text=The%20index%20is%20a%20binary,Git%3A%20they%20are%20used%20interchangeably.
+        /// </summary>
+        /// <param name="pattern"></param>
+        public void Add(string pattern)
+        {
+            WriteIndex(ReadIndex()
+                .Concat(Directory.EnumerateFiles(RootPath)
+                    .Select(x => $"{WriteObjectToObjectStore(x)} {x.Substring(RootPath.Length)}"))
+                .Distinct()
+                .OrderBy(x => x));
+        }
+
+        public string[] ReadIndex() => File.ReadAllLines(Path.Combine(RootPath, ".git/index"));
+        public void WriteIndex(IEnumerable<string> content) => File.WriteAllLines(Path.Combine(RootPath, ".git/index"), content);
+
+        public string WriteObjectToObjectStore(string path)
+        {
+            var content = File.ReadAllBytes(path);
+            var hash = ByteHelper.ComputeSha(content);
+            File.WriteAllBytes(Path.Combine(RootPath, $".git/objects/{hash}"), content);
+            return hash;
+        }
+
+        public string ReadObjectFromObjectStore(string hash) => File.ReadAllText(Path.Combine(RootPath, $".git/objects/{hash}"));
 
         public string Commit(string commitMessage)
         {
@@ -41,6 +71,37 @@ namespace KbgSoft.KBGit
         {
             return "";
         }
+
+        
     }
+
+    public static class ByteHelper
+    {
+        static readonly SHA256 Sha = SHA256.Create();
+
+        public static string ComputeSha(object o) => ComputeSha(Serialize(o));
+        public static string ComputeSha(byte[] b) => string.Join("", Sha.ComputeHash(b).Select(x => String.Format("{0:x2}", x)));
+
+        public static byte[] Serialize(object o)
+        {
+            using (var stream = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(stream, o);
+                stream.Seek(0, SeekOrigin.Begin);
+                return stream.GetBuffer();
+            }
+        }
+
+        public static T Deserialize<T>(Stream s) where T : class => (T)new BinaryFormatter().Deserialize(s);
+
+        public static T Deserialize<T>(byte[] o) where T : class
+        {
+            using (var ms = new MemoryStream(o))
+            {
+                return (T)new BinaryFormatter().Deserialize(ms);
+            }
+        }
+    }
+
 
 }
--- kbgit.tests/Git2Tests.cs
+++ kbgit.tests/Git2Tests.cs
@@ -4,49 +4,111 @@ using System.IO;
 using System.Text;
 using FluentAssertions;
 using KbgSoft.KBGit;
+using KbgSoft.KBGit2;
 using Xunit;
 
-namespace kbgit.tests
+namespace kbgit.tests2
 {
     public class Git2Tests
     {
-        Git2 MakeEmptyRepo()
-        {
-            var name = $@"c:\temp\kbggit{DateTime.Now.Ticks}";
-            Directory.CreateDirectory(name);
-            return new Git2(name);
-        }
+        private RepoBuilder builder = new RepoBuilder();
 
-        Git2 InitEmptyRepo()
-        {
-            var git = MakeEmptyRepo();
-            git.Init();
-            return git;
-        }
 
         [Fact]
         public void When_init_Then_create_git_folder()
         {
-            var git = MakeEmptyRepo();
-            
+            var git = builder.MakeEmptyRepo().Build();
+
             var msg = git.Init();
-                
+
             msg.Should().Be("Initialized empty Git repository");
             new DirectoryInfo(Path.Combine(git.RootPath, ".git")).Exists.Should().BeTrue();
             new DirectoryInfo(Path.Combine(git.RootPath, ".git/refs/heads")).Exists.Should().BeTrue();
             new DirectoryInfo(Path.Combine(git.RootPath, ".git/objects")).Exists.Should().BeTrue();
-            new DirectoryInfo(Path.Combine(git.RootPath, ".git/index")).Exists.Should().BeTrue();
+            File.Exists(Path.Combine(git.RootPath, ".git/index")).Should().BeTrue();
             File.ReadAllText(Path.Combine(git.RootPath, ".git/HEAD")).Should().Be("ref: refs/heads/master");
         }
 
         [Fact]
+        public void When_empty_repo_Then_index_is_empty()
+        {
+            var git = builder.InitEmptyRepo()
+                .WithFile("a.txt", "aaa").Build();
+
+            git.ReadIndex().Should().BeEquivalentTo(new string[0]);
+        }
+
+        [Fact]
+        public void When_staging_file_Then_added_to_the_index_and_stored_in_objectstore()
+        {
+            var git = builder.InitEmptyRepo()
+                .WithFile("a.txt", "aaa").Build();
+
+            git.Add("*");
+
+            git.ReadIndex().Should().BeEquivalentTo("9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0 a.txt");
+            git.ReadObjectFromObjectStore("9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0").Should().Be("aaa");
+        }
+
+        [Fact]
+        public void When_staging_add_all_files_Then_add_to_index()
+        {
+            var git = builder.InitEmptyRepo()
+                .WithFile("a.txt", "aaa")
+                .WithFile("b.txt", "aaa")
+                .Build();
+
+            git.Add("*");
+
+            git.ReadIndex().Should().BeEquivalentTo(
+                "9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0 a.txt",
+                "9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0 b.txt"
+                );
+        }
+
+
+        [Fact]
         public void When_list_branches_Then_return_empty()
         {
-            var git = InitEmptyRepo();
+            var git = builder.InitEmptyRepo().Build();
 
             new DirectoryInfo(Path.Combine(git.RootPath, ".git")).Exists.Should().BeTrue();
         }
 
 
     }
+
+    class RepoBuilder
+    {
+        string name = $@"c:\temp\kbggit{DateTime.Now.Ticks}\";
+
+        private Git2 Git;
+
+        public RepoBuilder MakeEmptyRepo()
+        {
+                Directory.CreateDirectory(name);
+                Git = new Git2(name);
+            return this;
+        }
+
+        public RepoBuilder InitEmptyRepo()
+        {
+                MakeEmptyRepo();
+                Git.Init();
+            return this;
+        }
+
+        public RepoBuilder WithFile(string path, string content)
+        {
+                File.WriteAllText(Path.Combine(name,path), content);
+            return this;
+        }
+
+        public Git2 Build()
+        {
+            return Git;
+        }
+
+    }
+
 }
